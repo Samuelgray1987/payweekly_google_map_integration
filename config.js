4f1abeabc72027cfac55d5c6a415f36a5c80c033
Samuel Gray <samuelgray1987@gmail.com>
2018-04-05 17:17:08 +0100

    rm things

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const config = require('./config.json');
 
 const env = process.env.NODE_ENV || 'development';
 
-if (env === 'development' || env === 'prod') {
+if (env === 'development' || env === 'test') {
     const envConfig = config[env];
     Object.keys(envConfig).forEach((key) => {
         process.env[key] = envConfig[key];
